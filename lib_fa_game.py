042307dbc73635194c4b5308d99ea777c410a358
ErikRacsson <62341886+ErikRacsson@users.noreply.github.com>
2020-03-18 08:05:41 -0600

    Create lib_fa_game.py
    Initial commit.

---
+++ lib_fa_game.py
@@ -0,0 +1,187 @@
+# lib_fa_game
+import random
+import types
+
+# Return codes for bad things.  TODO: Replace this construct with exception raising
+ERR_NAME_EXISTS = 1
+
+class characters:
+    names = {}
+    
+    def __init__(self, name, location, race="human"):
+        self.name = name
+        self.race = race
+        self.attributes = {}
+        self.__init_attributes()
+        characters.names[name] = self
+        self.location = "new"
+        location.enter(self)
+        self.holding = ""
+    
+    def __init_attributes(self):
+        self.attributes["strength"] = random.randint(1,10)
+        self.attributes["intelligence"] = random.randint(1,10)
+        self.attributes["dexterity"] = random.randint(1,10)
+    
+    def describe(self):
+        print("{} is a {}.".format(self.name, self.race))
+        for attribute in self.attributes:
+            print(f"\t{attribute} is {self.attributes[attribute]}")
+    
+    def createchar(name, location):
+        ''' attempt to create a new character with given name and place in the specified location
+            name - string representing the desired name for the character
+            location - location object where character should initially be placed
+            
+            returns character object reference or ERR_NAME_EXISTS if character name already exists.
+        '''
+        if name in characters.names:
+            return(ERR_NAME_EXISTS)
+        else:
+            return(characters(name, location))
+    
+    def move(self, port):
+        ''' move a character to a new location by way of a specified port (door) name
+        
+            port = string that is the name of the port in the location of the character
+        '''
+        valid_ports = self.location.ports
+        return_list = []
+        if port in valid_ports:
+            return_list.extend(valid_ports[port].use(self))
+        else:
+            return_list.append(f"There is no way you can reach the {port} here.")
+        return(return_list)
+        
+    def look_around(self):
+        ''' return a string list describing the character's environment '''
+        return_list = []
+        return_list.append(f'{self.name} is in the {self.location.name}')
+        return_list.extend(self.location.look())
+        return(return_list)
+    
+    def take(self, thing_to_take):
+        if isinstance(thing_to_Take, types.entityType):
+            self.holding = thing_to_take
+        
+
+    def count():
+        return(len(characters.names))
+        
+    def describechar(name):
+        if name in characters.names:
+            characters.names[name].describe()
+        else:
+            print("Sorry, can't find anyone named {}.".format(name))
+            
+    def list():
+        for character in characters.names :
+            print(f'{character} is in {characters.names[character].location.name}')
+
+
+class location:
+    names = dict()
+    
+    def __init__(self, name):
+        self.name = name
+        location.names[name] = self
+        self.description = "This is a generic location.  Everything is normal"
+        self.short_description = "Generic Location"
+        self.characters = dict()
+        self.ports = {}
+        self.things = []
+        
+    def look(self):
+        return_list = []
+        return_list.append(self.name)
+        return_list.append(self.description)
+        return_list.append(f"There are {len(self.characters)} characters here:")
+        for name in self.characters:
+            return_list.append(f'\t{name}')
+        return_list.append(f"There are {len(self.ports)} ways in and out of here:")
+        for port_name in self.ports:
+            return_list.append(f'\t{port_name}')
+        return(return_list)
+        
+    def enter(self, character):
+        return_list = []
+        if character.location != "new":
+            return_list.extend(character.location.exit(character))
+        return_list.append(f'{character.name} has entered the {self.name}')
+        return_list.append(f'{self.short_description}')
+        self.characters[character.name] = character
+        character.location = self
+        return(return_list)
+    
+    def exit(self, character):
+        return_list = []
+        del self.characters[character.name]
+        return_list.append(f'{character.name} has exited the {self.name}')
+        return(return_list)
+        
+    def look_chars(self):
+        for character in self.characters:
+            print(f'{character} is here')
+            
+    def describe_chars(self):
+        for character in self.characters:
+            character.describe()
+            
+    def list_verbose():
+        return_list = []
+        for loc in location.names :
+            return_list.append(f'{loc}')
+            for character in location.names[loc].characters:
+                return_list.append(f'\tA funloving character named {character}')
+        return(return_list)        
+        
+    def list():
+        return_list = []
+        for loc in location.names:
+            return_list.append(loc)
+        return(return_list)
+
+class ports:
+    __all = {}
+    def __init__(self, name):
+        self.name = name
+        self.description = "This is a generic port.  Everything is normal"
+        self.short_description = "Generic Port"
+        self.side_a = "empty"
+        self.side_b = "empty"
+        self.travel = False
+        ports.__all[name] = self
+        
+    def connect(self, side_a, side_b):
+        self.travel = True
+        self.side_a = side_a
+        self.side_b = side_b
+        side_a.ports[side_b.name+self.name] = self
+        side_b.ports[side_a.name+self.name] = self
+        
+    def use(self, character):
+        return_list = []
+        characters_location = character.location
+        if self.travel:
+            if characters_location == self.side_a:
+                return_list.extend(self.side_b.enter(character))
+            else:
+                return_list.extend(self.side_a.enter(character))
+        else:
+            return_list.append("The way is blocked!")
+        return(return_list)
+    
+    def list():
+        for port in ports.__all:
+            print(f'{port} is connecting {ports.__all[port].side_a.name} to {ports.__all[port].side_b.name}')
+
+class entity:
+    def __init__(self, name):
+        self.name = name
+        self.description = "Long description of an entity"
+        self.short_description = "Short description of an entity"
+     
+
+def format(list):
+    for line in list:
+        print(line)
