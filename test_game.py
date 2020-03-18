6fe7c506c04b671796d0d864bcb7ddeca8b52d11
ErikRacsson <62341886+ErikRacsson@users.noreply.github.com>
2020-03-18 11:01:39 -0600

    Erik (#4)
    * Update test_game.py
    
    Test Comment
    
    * Update test_game.py
    
    Test2  Should be a meaningful description to remind what we did and why.
    
    * Update test_game.py
    
    Fixed the lines that are over 80 chars per PEP8 standard.
    
    Co-authored-by: Bluefaira <mialorodeth@gmail.com>

--- test_game.py
+++ test_game.py
@@ -1,33 +1,44 @@
 import lib_fa_game as game
 
+#Test change to demonstrate github
+
 print("-- Creating map of locations with links")        
 mailbox = game.location("Mailbox")
 mailbox.short_description = "This is the inside of a mailbox"
-mailbox.description = ''' It is very dark and cramped.  Frankly it is leaving you wondering how you even got here.
-Very odd.  Anyway, looking around you can see what looks like steps leading to a patio...'''
+mailbox.description = \
+''' It is very dark and cramped.  Frankly it is leaving you wondering how you
+even got here.  Very odd.  Anyway, looking around you can see what looks like
+steps leading to a patio...'''
 
 shrub = game.location("Shrubbery")
 shrub.short_description = "This is the shrubbery"
-shrub.description = " It is a very nice shrubbery, neatly trimmed.  Or, rather it would be very nice if you could see \n \
-it.  As it is, you find yourself crouched down among the branches trying to avoid getting yourself too \n \
-dirty.  Looking around you notice that there does seem to be a way out..." 
+shrub.description = \
+''' It is a very nice shrubbery, neatly trimmed.  Or, rather it would be very
+nice if you could see it.  As it is, you find yourself crouched down among the
+branches trying to avoid getting yourself too dirty.  Looking around you
+notice that there does seem to be a way out...''' 
 
 patio = game.location("Patio")
 patio.short_description = "Lovely patio in front of house"
-patio.description = " The patio is made of tile and very well kept, although a bit barren.  I suppose having no \n \
-furniture (or anything else for that matter) does make it easier to clean up.  There is an ornate door \n \
-to the house here.  You can just make out the foyer through it."
+patio.description = \
+''' The patio is made of tile and very well kept, although a bit barren.  I
+suppose having no furniture (or anything else for that matter) does make it
+easier to clean up.  There is an ornate door to the house here.  You can just
+make out the foyer through it.'''
 
 foyer = game.location("Foyer")
 foyer.short_description = "Formal entrance room of the house"
-foyer.description = " This is a formal foyer.  There is a tapestry hanging on one wall that rather reminds you \n \
-of a palace, which this house certainly is not.  The ceiling is a bit low for the size of that wall hanging \n \
-and it makes it appear out of place.  The umbrella stand is empty, as is the coat rack.  There are doors \n \
-leading out of this room."
+foyer.description = \
+''' This is a formal foyer.  There is a tapestry hanging on one wall that
+rather reminds you of a palace, which this house certainly is not.  The
+ceiling is a bit low for the size of that wall hanging and it makes it
+appear out of place.  The umbrella stand is empty, as is the coat rack.
+There are doors leading out of this room.'''
 
 kitchen = game.location("Kitchen")
 kitchen.short_description = "House kitchen"
-kitchen.description = ''' This is the kitchen.  It is somewhat messy, but you may be able
+kitchen.description = \
+''' This is the kitchen.  It is somewhat messy, but you may be able
 to make a sandwich if you were to push the dirty dishes off the counter'''
 
 kitchen_door = game.ports("Door")
@@ -51,10 +62,13 @@ joe = game.characters.createchar("Joe",mailbox)
 player_name = input("What is your name? ")
 player_char = game.characters.createchar(player_name,mailbox)
 while player_char == game.ERR_NAME_EXISTS:
-    player_name = input(f"I'm sorry, {player_name} but someone with your name is already here.\nPlease choose another name")
+    player_name = input(f"I'm sorry, {player_name} but someone with your "\
+                        f"name is already here.\nPlease choose another name")
     player_char = game.characters.createchar(player_name,mailbox)
 
-commands_list = {'Look' : 'Look around', 'Go' : 'as in "go PatioSteps"', 'Exit' : 'as in exit the game', 'Commands' : 'as in give this list'}
+commands_list = {'Look' : 'Look around', 'Go' : 'as in "go PatioSteps"',
+                 'Exit' : 'as in exit the game',
+                 'Commands' : 'as in give this list'}
 
 def commands():
     print(f'You can choose from one of the following commands:\n')
